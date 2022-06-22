6b567e762d9defcc08d1029899fbced4a3462d25
Lu Wang <wang0631@algonquinlive.com>
2022-06-21 23:47:56 -0400

    Practice2

--- app/src/main/java/com/college/quiz1_question/MainActivity.java
+++ app/src/main/java/com/college/quiz1_question/MainActivity.java
@@ -1,7 +1,10 @@
 package com.college.quiz1_question;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText mShareTextEditText;
     //EditText for Dial action
     private EditText mDialEditText;
+    Button openWeb, openLoc,share, dial;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         mShareTextEditText = findViewById(R.id.share_edittext);
         mDialEditText = findViewById(R.id.phone_edittext);
 
+
     }
 
     // TODO
@@ -40,6 +46,17 @@ public class MainActivity extends AppCompatActivity {
      * @param view The view (Button) that was clicked.
      */
     public void openWebsite(View view) {
+        Button openWeb = findViewById(R.id.open_website_button);
+        openWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //把edittext变成string
+                String url = mWebsiteEditText.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
     }
 
     // TODO
@@ -51,7 +68,18 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param view The view (Button) that was clicked.
      */
+
     public void openLocation(View view) {
+        Button openLoc = findViewById(R.id.open_location_button);
+        openLoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
+                Uri locationUri = Uri.parse("geo:0?q=" + location);
+                Intent intent = new Intent(Intent.ACTION_VIEW,locationUri);
+                startActivity(intent);
+            }
+        });
 
     }
 
@@ -66,8 +94,23 @@ public class MainActivity extends AppCompatActivity {
      * @param view The view (Button) that was clicked.
      */
     public void shareText(View view) {
+        Button share = findViewById(R.id.share_text_button);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String context =  mShareTextEditText.getText().toString();
+                String Type = "text/plain";
+                ShareCompat.IntentBuilder.from(MainActivity.this)
+                        .setType(Type)
+                        .setText(context)
+                        .setChooserTitle("Share this content with")
+                        .startChooser();
+            }
+        });
 
     }
+
+
 
     // TODO
     /**
@@ -76,6 +119,17 @@ public class MainActivity extends AppCompatActivity {
      * @param view The view (Button) that was clicked.
      */
     public void dial(View view) {
+        Button dial = findViewById(R.id.dial_button);
+        dial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNo = mDialEditText.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+phoneNo));
+                startActivity(intent);
+            }
+        });
+
     }
 
     // TODO
