35c336ac17680fcb70ea5fe3c4c34d3f6e1d023a
Lu Wang <wang0631@algonquinlive.com>
2022-06-22 09:51:50 -0400

    practice1

--- app/src/main/java/com/college/quiz1_question/MainActivity.java
+++ app/src/main/java/com/college/quiz1_question/MainActivity.java
@@ -3,6 +3,7 @@ package com.college.quiz1_question;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -10,6 +11,10 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int mCounter;
+    Button pay, clear;
+    int mTotal;
+    int count;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         // This textview displays the result of multiplying
         // the content of the numberId EditText by 5
         TextView resultTv = findViewById(R.id.resultId);
+        Button pay = findViewById(R.id.btnPayId);
+        Button clear = findViewById(R.id.btnResetId);
 
         // TODO
         // Implement the 2 listeners for buttons 'clear' and 'pay'
@@ -29,6 +36,24 @@ public class MainActivity extends AppCompatActivity {
         // Result should contain 5 multiplied by number chosen
         // Display should be prefixed with the '$' sign such as $15
         // Reset should clear all the fields
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //edittext string to int
+                int count = Integer.parseInt(numberStr.getText().toString());
+                mTotal = count * 5;
+                //int to string
+                resultTv.setText(String.valueOf("$" + mTotal));
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberStr.setText(null);
+                resultTv.setText(String.valueOf(0));
+            }
+        });
 
     }
 }
